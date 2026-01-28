4b98b0b4f7c0124dff962310706bcd06406731ce
dimastatz <dima.statz@gmail.com>
2026-01-28 06:15:24 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+abort()
+{
+    echo "*** FAILED ***" >&2
+    exit 1
+}
+
+if [ "$#" -eq 0 ]; then
+    echo "No arguments provided. Usage: 
+    1. '-local' to build local environment
+    2. '-docker' to build and run docker container
+    3. '-test' to run linter, formatter and tests
+    4. '-benchmark' to run benchmark tests
+    5. '-run-server' to run fastapi server
+    6. '-setup' to run package setup"
+elif [ $1 = "-local" ]; then
+    trap 'abort' 0
+    set -e
+    echo "Running format, linter and tests"
+    rm -rf .venv
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install -r ./requirements.txt
+
+    black mindquest tests
+    pylint --fail-under=9.9 mindquest tests
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov mindquest -v tests
+elif [ $1 = "-test" ]; then
+    trap 'abort' 0
+    set -e
+    
+    echo "Running format, linter and tests"
+    source .venv/bin/activate
+    black mindquest tests
+    pylint --fail-under=9.9 mindquest tests
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov --log-cli-level=INFO mindquest -v tests
+elif [ $1 = "-docker" ]; then
+    echo "Building and running docker image"
+    docker stop mindquest-container
+    docker rm mindquest-container
+    docker rmi mindquest-image
+    # build docker and run
+    docker build --tag mindquest-image --build-arg CACHEBUST=$(date +%s) . --file Dockerfile.test
+    docker run --name mindquest-container -p 8888:8888 -d mindquest-image
+elif [ $1 = "-benchmark" ]; then
+    echo "Running mindquest Server"
+    kill $(lsof -t -i:8181) 
+    nohup uvicorn mindquest.fast_server:app --host 0.0.0.0 --port 8181 &
+    sleep 2s
+    echo "Running mindquest benchmark tests"
+    pytest -v -s tests/benchmark
+    kill $(lsof -t -i:8181)
+elif [ $1 = "-run-server" ]; then
+    echo "Running mindquest server"
+    kill $(lsof -t -i:8181) 
+    uvicorn mindquest.fast_server:app --host 0.0.0.0 --port 8181
+elif [ $1 = "-test-package" ]; then
+    echo "Running mindquest package setup"
+    # pip install twine
+    # pip install wheel
+    python setup.py sdist bdist_wheel
+    rm -rf .venv_test
+    python3 -m venv .venv_test
+    source .venv_test/bin/activate
+    pip install ./dist/mindquest-0.1.0-py3-none-any.whl
+    pytest --ignore=tests/benchmark --cov-fail-under=95 --cov mindquest -v tests
+    # twine upload ./dist/*
+else
+  echo "Wrong argument is provided. Usage:
+    1. '-local' to build local environment
+    2. '-docker' to build and run docker container
+    3. '-test' to run linter, formatter and tests
+    4. '-benchmark' to run benchmark tests
+    5. '-run-server' to run fastapi server
+    6. '-setup' to run package setup"
+fi
+
+trap : 0
+echo >&2 '*** DONE ***'

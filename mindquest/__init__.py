c34db86b740533b4467c6ae5f008c3ddd668db2c
dimastatz <dima.statz@gmail.com>
2026-02-01 19:48:19 +0200

    clean code base

--- mindquest/__init__.py
+++ mindquest/__init__.py
@@ -3,13 +3,3 @@
 A Python library that automates the end-to-end production of educational
 podcasts tailored for children aged 8-12.
 """
-
-from mindquest.script import create_script
-from mindquest.voice import voice_over
-
-__version__ = "0.1.0"
-
-__all__ = [
-    "create_script",
-    "voice_over",
-]
